5bd7ddeb3a6d509fed761d155417023c28ed5f8e
Deniz <Naxulanth@gmail.com>
2017-10-04 13:58:55 +0300

    Add files via upload

--- coin.cpp
+++ coin.cpp
@@ -1,3 +1,7 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <wiringPi.h>
 #include <curlpp/cURLpp.hpp>
@@ -5,12 +9,16 @@
 #include <curlpp/Options.hpp>
 #include <curlpp/Exception.hpp>
 #include <curlpp/Infos.hpp>
+#include <linux/input.h>
+#include <linux/uinput.h>
+#include <fstream>
+#include <iostream>
 
 /*
 A simple program to update total amount of coins inserted (whenever a coin
 insertion occurs) by sending a request to the API using curl library.
 
-Credit: Bilgi Open Source Community - Deniz G�lnar
+Credit: Bilgi Open Source Community - Deniz Gulnar
 Charity Arcade Project
 */
 
@@ -23,39 +31,118 @@ static curlpp::Cleanup clean;
 static int press = 0;
 
 // this is called when an interrupt happens (button press etc.)
-static void perform_() {;
-	press++;
+static void perform_() {
+    ;
+    press++;
+}
+
+void ioctl_problem_exit() {
+    printf("ioctl error");
+    exit(EXIT_FAILURE);
 }
 
 
 int main() {
-// initialize wiringPi, abort if it can't be initialized
-    if(wiringPiSetup() < 0)return 1;
-// initialize interrupt handler
-    if(wiringPiISR(PIN, INT_EDGE_FALLING, &perform_) < 0)
+
+    int total_coin = 0;
+    std::string rd = "";
+    std::ifstream r_file;
+    std::ofstream w_file;
+
+    int fd;
+    struct uinput_user_dev uidev;
+    struct input_event ev;
+
+
+    // initialize wiringPi, abort if it can't be initialized
+    if (wiringPiSetup() < 0) return 1;
+    // initialize interrupt handler
+    if (wiringPiISR(PIN, INT_EDGE_FALLING, &perform_) < 0)
     {
         printf("interrupt setup fail\n");
     }
-// if everything is working properly, create a curl request and set it up
-	printf("Ready\n");
- 		static curlpp::Easy request;
-                request.setOpt(curlpp::Options::Verbose(true));
-                request.setOpt(curlpp::Options::Url("TBD"));
-
-// main loop
-    while(1)
+    // if everything is working properly, create a curl request and set it up
+    printf("Ready\n");
+    std::list<std::string> header;
+    header.push_back("Authorization: Token bae7787eb9292caee617a72956f6ce9c253df773");
+    static curlpp::Easy request;
+    static curlpp::Forms form;
+    request.setOpt(curlpp::Options::HttpHeader(header));
+    request.setOpt(curlpp::Options::CustomRequest("PATCH"));
+    form.push_back(new curlpp::FormParts::Content("money", std::to_string(1)));
+    request.setOpt(curlpp::Options::HttpPost(form));
+    request.setOpt(curlpp::Options::Verbose(true));
+    request.setOpt(curlpp::Options::Url("ozguryazilim.bilgi.edu.tr/api/arcade/2/"));
+    printf("ok\n");
+    // main loop
+    while (true)
     {
-// execute code on interrupt
-        if(press)
+        // execute code on interrupt
+        if (press)
         {
-// do nothing until the pin voltage is back to normal (wait for button
-// to be released etc.)
-            while(digitalRead(PIN)==0);
-// perform curl request
-		request.perform();
-// flush output buffer
-		printf("\n");
-// reset interrupt state back to 0
+            fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+            if (fd < 0) {
+                printf("uniput init error");
+                exit(EXIT_FAILURE);
+            }
+
+            if (ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0) ioctl_problem_exit();
+
+            if (ioctl(fd, UI_SET_EVBIT, EV_SYN) < 0) ioctl_problem_exit();
+
+            if (ioctl(fd, UI_SET_KEYBIT, KEY_D) < 0) ioctl_problem_exit();
+
+            memset(&uidev, 0, sizeof(uidev));
+            snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-sample");
+            uidev.id.bustype = BUS_USB;
+            uidev.id.vendor = 0x1;
+            uidev.id.product = 0x1;
+            uidev.id.version = 1;
+
+            if (write(fd, &uidev, sizeof(uidev)) < 0) {
+                printf("write error");
+                exit(EXIT_FAILURE);
+            }
+            if (ioctl(fd, UI_DEV_CREATE) < 0) ioctl_problem_exit();
+
+            // do nothing until the pin voltage is back to normal (wait for button
+            // to be released etc.
+            while (digitalRead(PIN) == 1);
+            // perform curl request
+            memset(&ev, 0, sizeof(ev));
+            ev.type = EV_KEY;
+            ev.code = KEY_D;
+            ev.value = 1;
+            if (write(fd, &ev, sizeof(ev)) < 0) {
+                printf("write 2 error");
+                exit(EXIT_FAILURE);
+            }
+            memset(&ev, 0, sizeof(ev));
+            ev.type = EV_SYN;
+            ev.code = 0;
+            ev.value = 0;
+            if (write(fd, &ev, sizeof(ev)) < 0) {
+                printf("synchronize error");
+                exit(EXIT_FAILURE);
+            }
+            ioctl(fd, UI_DEV_DESTROY);
+            close(fd);
+            try {
+                request.perform();
+            }
+            catch (std::exception e) {
+                r_file.open("cointotal.txt");
+                r_file >> rd;
+                r_file.close();
+                total_coin = std::stoi(rd);
+                total_coin += 1;
+                w_file.open("cointotal.txt");
+                w_file << total_coin;
+                w_file.close();
+            }
+            // flush output buffer
+            printf("\n");
+            // reset interrupt state back to 0
             press = 0;
         }
     }
