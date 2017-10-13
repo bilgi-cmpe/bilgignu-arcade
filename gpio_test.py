2b02de709f6ad42cf5c3d29b92245984b426158b
orkunasa <orkunasa@gmail.com>
2017-10-13 14:29:09 +0300

    Python 3 print command fix :(

--- gpio_test.py
+++ gpio_test.py
@@ -5,7 +5,7 @@ from evdev import uinput, ecodes as e
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def my_callback(channel):
-    print "falling edge detected on 21"
+    print("falling edge detected on 21")
     with uinput.UInput() as ui:
          ui.write(e.EV_KEY, e.KEY_A, 1)
          ui.write(e.EV_KEY, e.KEY_A, 0)
